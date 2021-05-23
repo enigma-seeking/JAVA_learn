76fa5fe5280bb6c66824d9c95e89902c559b140e
enigma-seeking <youximailvip@163.com>
2021-05-23 16:34:27 +0800

    5.23学习

--- chapter1/src/chapter1.java
+++ chapter1/src/chapter1.java
@@ -1,5 +1,9 @@
 public class chapter1 {
     public static void main(String[] args) {
+        String greeting = "hello";
+        int n = greeting.length();
+        int cpCount = greeting.codePointCount(0, greeting.length());
+        System.out.println(cpCount);
 
     }
 
---
+++ test1.sql
@@ -0,0 +1,2 @@
+use myemployees;
+select commission_pct from employees where manager_id=100;
